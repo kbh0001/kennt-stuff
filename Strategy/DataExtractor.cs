e63d86a21b49d10af28d49153c6155471d1ae90c
Ken Hoskins <hoskinsken@hotmail.com>
2015-12-31 16:01:18 -0600

    Save updated DataExtractor

--- Strategy/DataExtractor.cs
+++ Strategy/DataExtractor.cs
@@ -1,10 +1,10 @@
+// This namespace holds all strategies and is required. Do not change it.
 using System;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 using Dapper;
-
-// This namespace holds all strategies and is required. Do not change it.
 using KenNinja;
 
 namespace NinjaTrader.Custom.Strategy
@@ -15,29 +15,11 @@ namespace NinjaTrader.Custom.Strategy
     [Description("Get Some Data")]
     public class DataExtractor : NinjaTrader.Strategy.Strategy
     {
-		public class DataItem {
-			public string Ver {get; set;}
-			public double High {get; set;}
-			public double Low {get; set;}
-			public double Open {get; set;}
-			public double Close {get; set;}
-			public double Volume {get; set;}
-			public DateTime DtTm {get;set;}
-			public DateTime Dt {get;set;}
-			public int Hr {get; set;}
-            public double CandleStick { get; set; }
-				
-		}
-			
-			
-		
-        
-        // Wizard generated variables
-        private int myInput0 = 1; // Default setting for MyInput0
         // User defined variables (add any user defined variables below)
-		
-		protected IDbConnection  DbConn;
-       
+
+        protected IDbConnection DbConn;
+        private int myInput0 = 1; // Default setting for MyInput0
+
 
         /// <summary>
         /// This method is used to configure the strategy and is called once before any strategy method is called.
@@ -45,62 +27,108 @@ namespace NinjaTrader.Custom.Strategy
         protected override void Initialize()
         {
             CalculateOnBarClose = false;
-			DbConn = OpenConnection();
+            DbConn = OpenConnection();
         }
-		
-		protected IDbConnection OpenConnection(){
-			try {
-				
-				IDbConnection conn = new SqlConnection("server=THEBEAST\\SQLEXPRESS;trusted_connection=yes;database=ninjaData;connection timeout=10;");
-				conn.Open();
-				return conn;
-				
-			}catch(System.Exception ex){
-				Print("DB Open Error" + ex.Message);
-				return null;
-			}
-		}
-			
+
+        protected IDbConnection OpenConnection()
+        {
+            try
+            {
+                IDbConnection conn =
+                    new SqlConnection(
+                        "server=THEBEAST\\SQLEXPRESS;trusted_connection=yes;database=ninjaData;connection timeout=10;");
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex)
+            {
+                Print("DB Open Error" + ex.Message);
+                return null;
+            }
+        }
+
 
         /// <summary>
         /// Called on each bar update event (incoming tick)
         /// </summary>
         protected override void OnBarUpdate()
         {
-            
-            var list = new[] { Kp.BullishHammer, Kp.BearishShootingStar, Kp.BullishHaramiCross, Kp.BearishHarami, Kp.BullishHarami, Kp.BullishDoji,  };
-
-            double candleStick = 0;
-
-            foreach (var dood in list)
+            var list = new[]
             {
-                if ((candleStick = this.KenCandleStickPattern(dood, 4)[0]) != 0)
+                Kp.BullishHammer, Kp.BearishShootingStar, Kp.BullishHaramiCross, Kp.BearishHarami, Kp.BullishHarami,
+                Kp.BullishDoji
+            };
+
+
+            var nonSignalRow = new DataItem
+            {
+                High = High[0],
+                Low = Low[0],
+                Open = Open[0],
+                Close = Close[0],
+                Volume = Volume[0],
+                DtTm = Time[0],
+                Dt = Time[0].Date,
+                Hr = Time[0].Hour,
+                CandleStick = 0,
+                IsSignal = 0
+            };
+            LogTheData(nonSignalRow);
+
+            foreach (var dood in Enum.GetValues(typeof(Kp)).Cast<Kp>())
+            {
+                double candleStick = KenCandleStickPattern(dood, 4)[0];
+                if (candleStick > 99 || candleStick < -99)
                 {
-                    break;
-
+                    var myRow = new DataItem
+                    {
+                        High = High[0],
+                        Low = Low[0],
+                        Open = Open[0],
+                        Close = Close[0],
+                        Volume = Volume[0],
+                        DtTm = Time[0],
+                        Dt = Time[0].Date,
+                        Hr = Time[0].Hour,
+                        CandleStick = candleStick,
+                        IsSignal = 1
+                    };
+                    LogTheData(myRow);
                 }
+
+
+
+
             }
-
-            
-            
-
-			var myRow = new DataItem{
-				High = High[0],
-				Low = Low[0],
-				Open = Open[0],
-				Close = Close[0],
-				Volume = Volume[0],
-				DtTm = Time[0],
-				Dt = Time[0].Date,
-				Hr = Time[0].Hour,
-                CandleStick = candleStick
-			};
-			
-			try{
-			DbConn.Execute("insert into dbo.extractedData([High], [Low], [Open], [Close], Dt, DtTm, Hr,  CandleStick) select @High, @Low, @Open, @Close, @Dt, @DtTm, @Hr, @CandleStick", myRow);
-			}catch (Exception ex){
-				Print("Fail " + ex.Message);
-			}
         }
-	}
+
+        private void LogTheData(DataItem myRow)
+        {
+            try
+            {
+                DbConn.Execute(
+                    "insert into dbo.extractedData([High], [Low], [Open], [Close], Dt, DtTm, Hr,  CandleStick, IsSignal) select @High, @Low, @Open, @Close, @Dt, @DtTm, @Hr, @CandleStick, @IsSignal",
+                    myRow);
+            }
+            catch (Exception ex)
+            {
+                Print("Fail " + ex.Message);
+            }
+        }
+
+        public class DataItem
+        {
+            public string Ver { get; set; }
+            public double High { get; set; }
+            public double Low { get; set; }
+            public double Open { get; set; }
+            public double Close { get; set; }
+            public double Volume { get; set; }
+            public DateTime DtTm { get; set; }
+            public DateTime Dt { get; set; }
+            public int Hr { get; set; }
+            public double CandleStick { get; set; }
+            public int IsSignal { get; set; }
+        }
+    }
 }
